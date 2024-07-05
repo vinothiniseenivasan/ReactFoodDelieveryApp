eb9d3c3103e104fb1f4af1a1b71357ec71bda1b1
vinothini <vinothiniciet@gmail.com>
2024-07-05 14:34:03 +0400

    make live api call from githhub in class based component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,19 +1,24 @@
 import User from "./User";
-import '../Css/about.css'
+import "../Css/about.css";
 import UserClass from "./UserClass";
-
-const About = () =>
-    {
-        return(
-            <div>
-            <h1>About page</h1>
-           
-            <User  name = "vino"/>
-            <UserClass  name ="vino class component" location="Dubai class" />
-          </div>
-        )
-        
-        
-    }
+import { Component } from "react";
+class About extends Component {
+  constructor() {
+    super();
+    // console.log("parent constructor")
+  }
+  componentDidMount() {
+    // console.log( "parent componentDidMount")
+  }
+  render() {
+    // console.log("parent render")
+    return (
+      <div>
+        <h1>About page</h1>
+        <UserClass name="first class " location="india" />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/Components/UserClass.jsx
+++ src/Components/UserClass.jsx
@@ -7,37 +7,48 @@ class UserClass extends React.Component {
     {
         super(props);
         this.state={
-          count: 0 ,
-          count2 : 3
+            userInfo :{
+              name : "null" ,
+              created_at:"0"
+            }
         }
-
-        console.log("constructor");
 
     }
     
+   async componentDidMount()
+  {
+    const response = await fetch("https://api.github.com/users/vinothiniseenivasan");
+    const json = await response.json();
+    console.log("json" ,json)
 
+    this.setState ({
+      userInfo :{
+        name : json.login ,
+        created_at:json.created_at
+      }
+    }
+     
+        
+    )
+  }
   render()
-  { 
-    console.log("render")
-    const {name , location} = this.props;
-   const {count,count2}  = this.state;
+   { 
+  
+    const {name ,created_at} = this.state.userInfo;
+  //  const {count,count2}  = this.state;
     return(
         <div className="user-card">
-          <button  onClick={()=>{
-              this.setState({
-                count : this.state.count+1
-              })
-            }} >
+          <button >
             Click Increase Count
           </button>
            
-                  <h1>{count}</h1>
+                  <h1></h1>
                  <h2>Name : {name}</h2>
-                 <h3>Location: {location}</h3>
+                 <h3>Location: {created_at}</h3>
                  <h4>Position : Front end developer</h4>
 
          </div>
-    )
+       )
 
   }
    
